07c97895d2d874206236392eb03fa4247283cb88
Madina Turtbayeva <madina.turtbaeva@gmail.com>
2019-09-09 00:51:12 -0400

    hello git!

--- src/practices/Repl_It.java
+++ src/practices/Repl_It.java
@@ -6,20 +6,18 @@ public class Repl_It {
 
 	public static void main(String[] args) {
 
+		Scanner input = new Scanner(System.in);
+	    System.out.println("Enter 2 numbers:");
+	    int num1 = input.nextInt();
+	    int num2 = input.nextInt();
 	
-	Scanner input = new Scanner(System.in);
-	System.out.println("Enter a number");
-	 int number = input.nextInt();
-	 
-	 if (number>0) {
-		 System.out.println(number+" is positive");
-	 }
-	 else if (number<0) {
-		 System.out.println(number+" is negative");
-	 }
-	 else System.out.println("it is zero");
-	 
-	 
+	    if(num1>num2) {
+	    	System.out.println(num1+" is greater than "+num2);
+	    }
+	    else if(num2>num1) {
+	    	System.out.println(num2+" is greater than "+num1);
+	    }
+	    else System.out.println(num1+" and "+num2+" are equal");
 	 
 	 
 	 
